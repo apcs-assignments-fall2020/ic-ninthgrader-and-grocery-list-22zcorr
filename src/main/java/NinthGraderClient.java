baa0c071e8035e582495c29d7b5eca84b83cff1c
22zcorr <22zcorr@athenian.org>
2020-11-02 19:46:20 +0000

    in class code

--- src/main/java/NinthGraderClient.java
+++ src/main/java/NinthGraderClient.java
@@ -1,3 +1,8 @@
 public class NinthGraderClient {
-    
+    public static void main(String[] args) {
+        NinthGrader a = new NinthGrader();
+        System.out.println(a);
+        a.doSomething();
+
+    }
 }
